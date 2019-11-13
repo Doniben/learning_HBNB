8170b86b79d9ba160265ac5b25dd752748366e4e
Doniben <doniben@esperanto.co>
2019-11-13 02:29:25 +0000

    [FEATURE] adding file storage, inits and new base_model

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ import json
 from models.base_model import BaseModel
 import os
 
+
 class FileStorage:
     """
     Class that serialize instances to a JSON file
@@ -24,19 +25,17 @@ class FileStorage:
     def save(self):
         """ Serialize Json file """
         dict_to_json = {}
-        for keys, in self.__objects:
-            dict_to_json[keys] = self.__objects[keys].to_dict()
-        print (dict_to_json)
+        for keys, value in self.__objects.items():
+            dict_to_json[keys] = value.to_dict()
         with open(self.__file_path, "w") as file:
-            file.write(json.dumps(dict_to_json)
+            file.write(json.dumps(dict_to_json))
 
     def reload(self):
         """ Deserialize JSON file """
         new_dict = {"BaseModel": BaseModel}
         """ Dictionary to able the reload """
-        if os.path.isfile(self.__file_path)
+        if os.path.isfile(self.__file_path):
             with open(self.__file_path, "r") as readed:
                 new_data = json.loads(readed.read())
-                for key values in new_data.items():
-                       self.__objects[key] = new_data[value["__class__"]]
-                       (**values)
+                for key, value in new_data.items():
+                    self.__objects[key] = new_dict[value["__class__"]](**value)
